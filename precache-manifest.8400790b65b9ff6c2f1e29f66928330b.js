bc93a4b4b0bbf05b5576eecd1d40b9ebcda5a68a
jorgevitiello <jorgevitiello@users.noreply.github.com>
2022-08-29 21:14:10 +0000

    deploy: 27141b766c94ba548c09ad99e29eb77fc5356cfd

---
+++ precache-manifest.8400790b65b9ff6c2f1e29f66928330b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3d87580759c45ee02be8a05a7e5de00",
+    "url": "https://jorgevitiello.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "bf26f6435105a94d6ec1",
+    "url": "https://jorgevitiello.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "913410a448650690823a",
+    "url": "https://jorgevitiello.github.io/task-management-frontend/static/js/2.de05c31b.chunk.js"
+  },
+  {
+    "revision": "bf26f6435105a94d6ec1",
+    "url": "https://jorgevitiello.github.io/task-management-frontend/static/js/main.cb170a00.chunk.js"
+  },
+  {
+    "revision": "25b690abd53769d3a7bc",
+    "url": "https://jorgevitiello.github.io/task-management-frontend/static/js/runtime~main.79febc4a.js"
+  }
+]);
